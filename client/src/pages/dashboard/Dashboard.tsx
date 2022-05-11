dce9af4d4f4699b3aeae1dfa74b0c747c048b599
shethsh1 <shaahid.sheth@mail.utoronto.ca>
2022-05-10 23:18:39 -0400

    .

--- client/src/pages/dashboard/Dashboard.tsx
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -1,16 +1,11 @@
 import * as React from 'react';
 
-import { styled, useTheme } from '@mui/material/styles';
+import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import Header from '../../components/dashboard/Header'
 import Home from '../../components/dashboard/Home'
 import SidebarContainer from '../../components/dashboard/SidebarContainer'
-import Constants from '../../constants/Constants'
-
-import { useAppDispatch, useAppSelector } from '../../redux/hooks'
-import type { user } from '../../redux/slices/authSlice'
-import { authLogout } from '../../redux/slices/authSlice'
 import { Main, DrawerHeader } from './StyledComponents'
 
 export default function Dashboard() {
